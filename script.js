1cefc0b3c6327c079d3807e33523c5777027c117
Felipe Mota <felipe.msrb@gmail.com>
2022-07-27 11:18:59 +0100

    fazendo o mario pular infinitamente

--- script.js
+++ script.js
@@ -3,8 +3,14 @@ const mario = document.querySelector('.mario');
 const pula = () => {
 mario.classList.add('jump');
 
+
+
+setTimeout(() => {
+    
+    mario.classList.remove('jump');
+
+}, 500);
+
 };
-
-
 
 document.addEventListener('keydown', pula);
